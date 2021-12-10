189cac033d11d7983683af388893f0d46837c55c
derplakatankleber <megakeil@gmx.net>
2021-12-10 15:29:29 +0100

    deploy with github secret

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { exec } = require('child_process');
 // let outputFilename= "firebase.js";
 const dev = process.env.NODE_ENV !== 'production'
 console.log("node env: '"+process.env.NODE_ENV+"'");
-let fb_config=${{secrets.FIREBASE_CONFIG}};
+let fb_config=process.env.FIREBASE_CONFIG;
 console.log("firebase config: '"+fb_config+"'");
 if(fb_config){
     fw.writeFileSync(path.resolve(__dirname, 'firebase-config.json'), fb_config);
