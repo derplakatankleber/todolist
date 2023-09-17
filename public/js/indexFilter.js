bd3cfed0db849d72b55601a5f7d6f6dea8a9cdc7
derplakatankleber <megakeil@gmx.net>
2023-09-17 15:25:14 +0200

    Update filterIndexInput

--- public/js/indexFilter.js
+++ public/js/indexFilter.js
@@ -1,12 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-	$(".indexFilter").on("input", function() {
-		let textvalue = $(".indexFilter")[0].value.toLowerCase();
-		$(".indexContainer button").each(function( i ) {
-			if($(this).text().toLowerCase().indexOf(textvalue)>-1){
-				$(this).show();
-			}else{
-				$(this).hide();
-			}
-		});
+function filterIndexInput() {
+	let textvalue = $(".indexFilter")[0].value.toLowerCase();
+	$(".indexContainer button").each(function (i) {
+		if ($(this).text().toLowerCase().indexOf(textvalue) > -1) {
+			$(this).show();
+		} else {
+			$(this).hide();
+		}
 	});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	$(".indexFilter").on("input", filterIndexInput);
+	setTimeout(filterIndexInput, 2000);
 });
